55aa9718e18cc3d506953ec95db006d2b797cf86
Monica Souza <monica.souza@eficode.com>
2021-10-06 14:20:41 +0200

    Ensure the connections are private

--- windows-config.ps1
+++ windows-config.ps1
@@ -4,6 +4,8 @@ $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 powershell.exe -ExecutionPolicy ByPass -File $file
 
+Set-NetConnectionProfile -InterfaceIndex 4 -NetworkCategory 'Private'
+
 winrm quickconfig
 
 Set-Item -Force WSMan:\localhost\Client\Allowunencrypted $True
