d0bf49dbb67f51c8d6863d98918275f19b48714b
Monica Souza <monica.souza@eficode.com>
2021-10-15 12:16:44 +0200

    Disable basic auth inside windows-config script

--- windows-config.ps1
+++ windows-config.ps1
@@ -2,7 +2,7 @@ $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-powershell.exe -ExecutionPolicy ByPass -File $file
+powershell.exe -ExecutionPolicy ByPass -File $file -DisableBasicAuth
 
 winrm quickconfig
 
